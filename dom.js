34848d84398e989a5f63d48eff73b0ce564e265e
Godwin254 <godwinbee15@gmail.com>
2020-01-09 09:01:05 +0300

    designed login page

---
+++ dom.js
@@ -0,0 +1,12 @@
+//console.log(document.head)
+//console.log(document.header);
+//alert('welcome').getElementById = "btx";
+//document.getElementById("meko").innerHTML = "Welcome hear";
+//console.log(meko);
+//var formList = document.querySelector('#container');
+
+document.documentElement; //HTMLhtmlElement
+document.head;
+document.body;
+document.documentElement.children[1].children[0];
+console.log("hello world");
